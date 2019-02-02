c221e45f03dc0d090c37f778b05095dae87b5310
1768181 <47192783+1768181@users.noreply.github.com>
2019-02-01 19:16:00 -0600

    Create practica 3.js

---
+++ practica 3.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+	fill(255,0,0);
+	noStroke();
+  ellipse(200,200,200,200);
+  fill(255);
+	noStroke();
+  ellipse(200,200,160,160);
+  fill(255,0,0);
+	stroke(255,0,0);
+  strokeWeight(25);
+  line(140,140,260,260);
+  
+}
