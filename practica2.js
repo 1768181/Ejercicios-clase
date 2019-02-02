618a8a482b2475e52c4e657b6a0ff74f408e6821
1768181 <47192783+1768181@users.noreply.github.com>
2019-02-01 19:14:51 -0600

    Create practica2.js

---
+++ practica2.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(0,255,0);
+  fill(255,0,0);
+	noStroke();
+  rect(0,0,200,600);
+  fill(0,0,255);
+	noStroke();
+  rect(400,0,200,600);
+}
